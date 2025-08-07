a41f06cfcc2beb7eb92536b2a01f738522d9dbc2
KIJ <dlrwn0730@naver.com>
2025-08-07 09:12:50 +0900

    a

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+const characterRouter = require('./routes/character')
+
+app.use(express.json())
+app.use('/char',characterRouter)
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello Express!")
+})
+app.listen(PORT, () => {
+    console.log("Server is running!")
+})
